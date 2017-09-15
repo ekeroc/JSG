3a1082ab4827288342199f16c3273c27effe9cd8
LangChuan Huang <er230059@gmail.com>
2017-09-16 00:33:59 +0800

    Add new api and complete login api (Issue: JSG-001)

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
 var bodyParser = require('body-parser');
 var express = require('express');
-var LocalStrategy = require('passport-local').Strategy;
 var logger = require('morgan');
 var passport = require('passport');
+var jwtStrategy = require('passport-jwt').Strategy;
+var extractJwt = require('passport-jwt').ExtractJwt;
+var config = require('./config.js');
+var userDatas = require('./users.json');
 
-var api = require('./routes/api')
+var api = require('./routes/api');
+var users = require('./routes/users');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-passport.use('local', new LocalStrategy(
-  function(username, password, done) {
-    if(username == 'testing' && password == '1234eszxcv') {
-        return done(null, {}, {'status': 'ok', 'token': 'VBa35EWz4v31Pw2V4mIemGwR'});
-    } else {
-        return done(null, {}, {'status': 'error'});
+var options = {
+  jwtFromRequest: extractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: config.jwtSecret,
+}
+passport.use(new jwtStrategy(options, function(jwt_payload, done) {
+  var user;
+  for (var i = 0; i < userDatas.length; i++) {
+    if (userDatas[i]['id'] == jwt_payload.sub) {
+      user = userDatas[i];
+      return done(null, user);
     }
   }
-));
+  return done(null, false);
+}));
 
 app.use(passport.initialize());
 
 app.use('/api', api);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    console.log(err);
-    res.send('error');
+  console.log(err);
+  res.send('error');
 });
 
 module.exports = app;
