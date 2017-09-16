94d847c4c9408eb52bd92b6c1f7427cc4307caa6
LangChuan Huang <er230059@gmail.com>
2017-09-16 21:58:41 +0800

    Modify error handle (Issue: JSG-000)

--- app.js
+++ app.js
@@ -35,21 +35,8 @@ app.use(passport.initialize());
 app.use('/api', api);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  console.log(err);
-  res.send('error');
+  res.sendStatus(404);
 });
 
 module.exports = app;
